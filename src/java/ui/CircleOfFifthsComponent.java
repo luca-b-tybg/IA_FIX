93d3b71f69aef91fde0c1790f6ddd9f9c3aa44f9
luca-b-tybg <19bulavitchi_l@bexleygs.co.uk>
2025-07-11 18:07:42 +0100

    fixed circle selection

--- src/java/ui/CircleOfFifthsComponent.java
+++ src/java/ui/CircleOfFifthsComponent.java
@@ -15,6 +15,8 @@ import java.util.Set;
 public class CircleOfFifthsComponent extends JPanel {
     private static final int RADIUS_ADJUSTMENT = 60;
     public List<CircleOfFifthsKeyFile> selectedKeys = new ArrayList<>();
+    public CircleOfFifthsKeyFile topSelectedKey = null;
+    public CircleOfFifthsKeyFile secondSelectedKey = null;
 
 
     public CircleOfFifthsComponent() {
@@ -35,8 +37,8 @@ public class CircleOfFifthsComponent extends JPanel {
         renderLabels(CircleOfFifthsGenerator.DIMINISHED_KEYS, 3);
     }
 
-    private Set<CircleOfFifthsKeyFile> getAllSelected () {
-        Set <CircleOfFifthsKeyFile> all = new HashSet<>();
+    private Set<CircleOfFifthsKeyFile> getAllSelected() {
+        Set<CircleOfFifthsKeyFile> all = new HashSet<>();
         for (CircleOfFifthsKeyFile keyFile : selectedKeys) {
             all.addAll(CircleOfFifthsGenerator.select(keyFile));
         }
@@ -44,37 +46,48 @@ public class CircleOfFifthsComponent extends JPanel {
     }
 
     private boolean isSelectedRoot(CircleOfFifthsKeyFile keyFile) {
-        return selectedKeys.contains(keyFile);
+
+        return (topSelectedKey != null && topSelectedKey.equals(keyFile))
+                ||
+                (secondSelectedKey != null && secondSelectedKey.equals(keyFile));
+
+    }
+
+    private void selectKey(CircleOfFifthsKeyFile keyFile) {
+        if(topSelectedKey == null) {
+            topSelectedKey = keyFile;
+        } else {
+            secondSelectedKey = keyFile;
+        }
+        selectedKeys.add(keyFile);
+        repaint();
     }
 
     public void renderLabels(CircleOfFifthsKeyFile[] keys, int circleLevel) {
 
-        Set <CircleOfFifthsKeyFile> selectedKeys = getAllSelected();
+        Set<CircleOfFifthsKeyFile> selectedKeys = getAllSelected();
         for (int i = 0; i < keys.length; i++) {
-            var scale = keys[i];
+            var currentKey = keys[i];
             double angle = Math.toRadians((360 / 12.) * i - 90);
-            var keyLabel = new JLabel(scale.toString());
+            var keyLabel = new JLabel(currentKey.toString());
             keyLabel.setFont(new Font("Default", Font.BOLD, 14));
             keyLabel.setSize(50, 16);
-            if (isSelectedRoot(scale)) {
+            if (isSelectedRoot(currentKey)) {
                 keyLabel.setForeground(Color.RED);
                 keyLabel.setFont(new Font("Default", Font.BOLD, 16));
-            } else
-
-            if (selectedKeys.contains(scale)) {
+            } else if (selectedKeys.contains(currentKey)) {
                 keyLabel.setForeground(new Color(33, 36, 209));
             }
 
             int xMaj = (int) (getWidth() / 2. + Math.cos(angle) * (getRadius(circleLevel) - 10));
             int yMaj = (int) (getHeight() / 2. + Math.sin(angle) * (getRadius(circleLevel) - 10));
             keyLabel.setLocation(xMaj, yMaj);
-            if (!scale.isDiminished()) {
+            if (!currentKey.isDiminished()) {
                 keyLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                 keyLabel.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        CircleOfFifthsComponent.this.selectedKeys.add( scale);
-                        repaint();
+                        CircleOfFifthsComponent.this.selectKey(currentKey);
                     }
 
 
@@ -107,6 +120,8 @@ public class CircleOfFifthsComponent extends JPanel {
 
     public void reset() {
         selectedKeys.clear();
+        topSelectedKey = null;
+        secondSelectedKey = null;
         repaint();
     }
 }
